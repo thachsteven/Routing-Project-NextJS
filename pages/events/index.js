80a29a2102fe1372ba726e8b797bbcc14ada2347
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-01-04 17:44:45 +0700

    Layout and Get All Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { getAllEvents } from '../../dummy-data'
+import EventList from "../../components/events/event-list"
 
 const AllEventsPage = () => {
+    const events = getAllEvents()
+
     return (
         <div>
-            All Events Page
+            <EventList items={events} />
         </div>
     )
 }
